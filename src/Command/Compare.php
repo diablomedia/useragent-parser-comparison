23440e4dcf485bea2419ee84ab0f5b92b84c564b
t.mueller <thomas.mueller@geld.de>
2019-03-08 09:07:44 +0100

    improve mapping

--- src/Command/Compare.php
+++ src/Command/Compare.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Compare extends Command
