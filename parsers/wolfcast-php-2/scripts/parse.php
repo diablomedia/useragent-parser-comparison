91a6a274cc4dbb5fe727a33c6c685398d3f48273
Thomas Müller <mimmi20@live.de>
2019-03-08 20:54:27 +0100

    add Wolfcast BrowserDetector (#3)
    * add Wolfcast BrowserDetector
    
    * improve mapping
    
    * style fix

---
+++ parsers/wolfcast-php-2/scripts/parse.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types = 1);
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', '-1');
+
+$benchmarkPos = array_search('--benchmark', $argv);
+$benchmark    = false;
+
+if ($benchmarkPos !== false) {
+    $benchmark = true;
+    unset($argv[$benchmarkPos]);
+    $argv = array_values($argv);
+}
+
+$agentListFile = $argv[1];
+
+$results   = [];
+$parseTime = 0;
+
+$start = microtime(true);
+require __DIR__ . '/../vendor/autoload.php';
+$parser   = new \BrowserDetection('Test String');
+$initTime = microtime(true) - $start;
+
+$file = new SplFileObject($agentListFile);
+$file->setFlags(SplFileObject::DROP_NEW_LINE);
+
+while (!$file->eof()) {
+    $agentString = $file->fgets();
+
+    if (empty($agentString)) {
+        continue;
+    }
+
+    $start  = microtime(true);
+    $result = new \BrowserDetection($agentString);
+    $end    = microtime(true) - $start;
+
+    $parseTime += $end;
+
+    if ($benchmark) {
+        continue;
+    }
+
+    $results[] = [
+        'useragent' => $agentString,
+        'parsed'    => [
+            'browser' => [
+                'name'    => $result->getName(),
+                'version' => $result->getVersion(),
+            ],
+            'platform' => [
+                'name'    => $result->getPlatform(),
+                'version' => $result->getPlatformVersion(true),
+            ],
+            'device' => [
+                'name'     => null,
+                'brand'    => null,
+                'type'     => null,
+                'ismobile' => $result->isMobile(),
+            ],
+        ],
+        'time' => $end,
+    ];
+}
+
+$file = null;
+
+$memory = memory_get_peak_usage();
+
+// Get version from composer
+$package = new \PackageInfo\Package('wolfcast/browser-detection');
+
+echo json_encode([
+    'results'     => $results,
+    'parse_time'  => $parseTime,
+    'init_time'   => $initTime,
+    'memory_used' => $memory,
+    'version'     => $package->getVersion(),
+], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
